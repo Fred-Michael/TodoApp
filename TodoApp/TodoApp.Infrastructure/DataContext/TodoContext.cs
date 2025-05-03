51339a5b1161727294b0d085323c75a90c458567
Chinazor Okafor <chinazorfredrick@gmail.com>
2025-05-03 18:43:14 -0400

    adding dbContext and registering the service

---
+++ TodoApp/TodoApp.Infrastructure/DataContext/TodoContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.TodoApp.Domain;
+
+namespace TodoApp.TodoApp.Infrastructure.DataContext
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base (options) { }
+
+        public DbSet<TodoItem> Todos { get; set; }
+    }
+}
